d3ddd6195efe1870d11a36bc0489c3a547078174
franarroyo <frankarroyop@gmail.com>
2021-03-12 08:16:03 +0100

    Ejercicio 3 y 4

---
+++ matrices/Ejercicio3.java
@@ -0,0 +1,80 @@
+package ejercicios;
+import java.util.Random;
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//3. Genera un programa que dadas dos matrices calcule su suma. 
+//Ten en cuenta que para que dos matrices se puedan sumar, éstas deben 
+//tener la misma dimensión (número de filas , número de columnas).Por ejemploDados 
+//[1, 0, 0]
+//[7, 14, 23]
+//[8, 14, 23]
+//		
+//[0, 1, 0] + [3,  9,  15]=
+//
+//[3, 10, 15]
+//[0, 0, 1]
+//[0,   7, 22]
+//[9,  7,  23]
+		//Se inician las dos matrices
+		int[][] matriz1= new int[3][3];
+		int[][] matriz2=new int[3][3];
+        int[][] suma = new int[matriz1.length][matriz2.length];
+        RellenadorDeMatrices(matriz1);
+        RellenadorDeMatrices(matriz2);
+        for (int i=0; i < matriz1.length; i++) {
+        	  for (int j=0; j< matriz1[i].length; j++) {				
+        	    suma[i][j]=matriz1[i][j]+matriz2[i][j];								
+        	  }
+        	}
+        System.out.println(VerMatrizConFormato(suma));
+	
+	}
+	private static void RellenadorDeMatrices(int[][] matriz1) {
+		Random asignadornumeros=new Random();
+		for (int i=0;i<matriz1.length;i++) {
+			for (int j=0;j<matriz1[i].length;j++) {
+				matriz1[i][j]=asignadornumeros.nextInt(10);
+			}
+		}
+	}
+    private static String VerMatrizConFormato(int[][] matriz){
+    	String matrizconformato="";
+    	int z=0;
+        for(int i=0;i<matriz.length;i++){
+        	matrizconformato=matrizconformato+"\n [";
+            for(int j=0;j<matriz[i].length;j++){
+            	if(j!=matriz[j].length-1) {
+            		matrizconformato+=matriz[i][j]+",";
+            	}
+        		else {
+        			matrizconformato+=matriz[i][j]+"]";
+        		}
+            }
+        }
+        return matrizconformato;
+    }
+    private static int[][] SumarMatriz(int[][] matriz1, int[][] matriz2){
+        int[][] matrizSuma;
+        int filas1 =matriz1.length;
+        int columnas1 = matriz1[0].length;
+        int filas2 =matriz2.length;
+        int columnas2 = matriz2[0].length;
+         
+        if(filas1 == filas2 && columnas1 == columnas2){
+            matrizSuma = new int[filas1][columnas1];
+            for(int i=0;i<matriz1.length;i++){
+                for(int j=0;j<matriz1[0].length;j++){
+                    matrizSuma[i][j]= matriz1[i][j] + matriz2[i][j];
+                }
+            }
+            return matrizSuma;
+        } else {
+            return null;
+        }
+    }
+ 
+
+
+}
---
+++ matrices/Ejercicio4.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//4. Elabora un programa que concatene listas de datos en una sola variable.
+//Por ejemplo, si recibe dos listas de tamaño 2 y 3 con los datos  [3, 2] y [5, 15, 23] 
+//devuelva una única lista de tamaño 5 con [3, 2, 5, 15, 23].
+	}
+
+}
