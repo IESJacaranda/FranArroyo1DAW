7b55150e3462d848e419d3558b2f0daa7fddae94
franarroyo <frankarroyop@gmail.com>
2021-03-16 14:20:42 +0100

    assd

---
+++ matrices/Ejercicio6.java
@@ -0,0 +1,53 @@
+package ejercicios;
+
+import java.util.Arrays;
+
+public class Ejercicio6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] matriz = {{1,2,3},{6,5}};
+		System.out.println(VerMatrizConFormato(RotarMatriz(matriz)));
+	}
+    public static String VerMatrizConFormato(int[][] matriz){
+    	String matrizconformato="";
+    	int z=0;
+        for(int i=0;i<matriz.length;i++){
+        	matrizconformato=matrizconformato+"\n [";
+            for(int j=0;j<matriz[i].length;j++){
+            	if(j!=matriz[j].length-1) {
+            		matrizconformato+=matriz[i][j]+",";
+            	}
+        		else {
+        			matrizconformato+=matriz[i][j]+"]";
+        		}
+            }
+        }
+        return matrizconformato;
+
+}
+    public static boolean CompararMatrices(int [][]matriz,int matriz2[][]) {
+    	boolean soniguales=false;
+    	if ((matriz.length==matriz2.length && matriz[0].length==matriz2[0].length)) {
+    		int [][] matrizDerecha=RotarMatriz(matriz);
+    		int [][] matrizAbajo=RotarMatriz(matrizDerecha);
+    		int [][] matrizIzquierda=RotarMatriz(matrizAbajo);
+    	}
+    	for (int i=0;i<matriz.length;i++) {
+    		for (int j=0;i<matriz2[i].length;j++) {
+    			soniguales=true;
+    		}
+    	}
+    	return soniguales;
+    }
+    public static int [][] RotarMatriz(int [][]matriz){
+		int aux = matriz.length;
+		int[][] nuevamatriz = new int[aux][aux];
+		for (int i=0;i<aux;i++) {
+			  for (int j=0;j<aux;j++) {
+				  nuevamatriz[i][aux-1-j] = matriz[i][j];
+			  }
+			}
+		return nuevamatriz;
+    }
+}
