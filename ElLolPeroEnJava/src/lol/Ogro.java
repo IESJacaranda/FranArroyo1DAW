95f3a0d5aacfb3c27ae66a321f99f80355b3514a
Fran <xXxArroyoxXx>
2021-03-23 16:02:12 +0100

    asd

--- ElLolPeroEnJava/src/lol/Ogro.java
+++ ElLolPeroEnJava/src/lol/Ogro.java
@@ -1,7 +1,7 @@
 package lol;
 
 public class Ogro extends Jugador {
-
+	@Override
 	public Ogro(char simbolo, int fuerza, int magia, int velocidad, int col, int dinero, int pociones, int gemas) {
 		super(simbolo, fuerza, magia, velocidad, col, dinero, pociones, gemas);
 		this.setVelocidad(4);
