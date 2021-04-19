7a66214571d110c5b5c936ebd25001a75a7b1ebe
franarroyo <frankarroyop@gmail.com>
2021-04-19 14:09:56 +0200

    agora siÇ

---
+++ TestVideojuegosPelis/src/VideoSeries/Entregable.java
@@ -0,0 +1,13 @@
+package VideoSeries;
+
+
+public interface Entregable {
+	  
+    public void entregar();
+  
+    public void devolver();
+  
+    public boolean isEntregado();
+  
+    public int compareTo(Object a);
+}
